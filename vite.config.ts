cff7665266ab4d2ac09ba0d5fb9f2ede03cd05b4
Zine76 <zine.d.chergui@gmail.com>
2024-11-18 00:45:16 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+export default {
+  build: {
+    rollupOptions: {
+      external: ['/src/main.tsx']
+    }
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
